76574cb841e5d99a42faefb567e074a1b1379aac
Denis <wolf-den@tut.by>
2021-02-20 10:03:12 +0300

    feat: all function for task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (typeof array === 'undefined' || array.length === 0) return 0;
+  let minNumberInArr;
+  for (let i = 0; i < array.length; i++) {
+    if (minNumberInArr === undefined) {
+      minNumberInArr = array[i];
+    } else {
+      if (minNumberInArr >= array[i]) {
+        minNumberInArr = array[i];
+      }
+    }
+  }
+  return minNumberInArr;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (typeof array === 'undefined' || array.length === 0) return 0;
+  let maxNumberInArr;
+  for (let i = 0; i < array.length; i++) {
+    if (maxNumberInArr === undefined) {
+      maxNumberInArr = array[i];
+    } else {
+      if (maxNumberInArr <= array[i]) { // <= for max
+        maxNumberInArr = array[i];
+      }
+    }
+  }
+  return maxNumberInArr;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (typeof array === 'undefined' || array.length === 0) return 0;
+  let avgNumberInArr, count = 1;
+  for (let i = 0; i < array.length; i++) {
+    if (avgNumberInArr === undefined) {
+      avgNumberInArr = array[i];
+    } else {
+      avgNumberInArr += array[i];
+      count++;
+    }
+  }
+  return avgNumberInArr / count;
 }
